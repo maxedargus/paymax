b0a83028318ab3d35c04d9d240f317b181cd146b
maxedargus <70198792+maxedargus@users.noreply.github.com>
2020-08-27 00:03:34 +0100

    Initial commit

---
+++ app/src/main/java/com/scienceboss/paymax/MainActivity.java
@@ -0,0 +1,42 @@
+package com.scienceboss.paymax;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.preference.PreferenceManager;
+import android.service.autofill.UserData;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.LibraryContent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.view.View.VISIBLE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+}
